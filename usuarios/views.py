d642f6ffbb24ffba6c57499da326a192a1f1a5b0
Igor <igor.warlog@gmail.com>
2024-03-21 23:33:25 -0300

    Criação do template base.htl, migração do db usuários e criação do html cadastro, e definições da views

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def cadastro(request):
+    
+    return render(request, 'cadastro.html')
+
+def valida_cadastro(request):
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+
+    return HttpResponse(f'{nome} {email} {senha}')
